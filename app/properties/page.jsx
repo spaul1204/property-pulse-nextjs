1d1bf7b1d2fd2680fa6cf4b90659dddde7839cfe
Sneha Paul <spaul1204@gmail.com>
2024-09-27 17:31:21 +0530

    added properties page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import properties from "@/properties.json";
+import Property from "@/components/Property";
 
-const Properties = () => {
-  return <div>Properties</div>;
+const PropertiesPage = () => {
+  //   console.log("properties ", properties);
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <div>No properties found</div>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <Property key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
-export default Properties;
+export default PropertiesPage;
