d85591106bfa9af3fab7d8de711e6e7351b777f2
Sneha Paul <spaul1204@gmail.com>
2024-09-27 18:02:25 +0530

    added loader

---
+++ app/loading.jsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const LoadingPage = () => {
+  const override = {
+    display: "block",
+    margin: "100px auto",
+  };
+  return (
+    <ClipLoader
+      color="#3b82f6"
+      cssOverride={override}
+      size={150}
+      aria-label="Loading spinner"
+    />
+  );
+};
+
+export default LoadingPage;
