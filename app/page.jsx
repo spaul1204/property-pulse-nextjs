3544f34d8c7d504ffce8c62de2619674f25ba672
Sneha Paul <spaul1204@gmail.com>
2024-09-27 17:46:40 +0530

    added view all properties page

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import "@/assets/styles/globals.css";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
+
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 };
