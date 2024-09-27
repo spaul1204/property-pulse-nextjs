1418176a48c17a9148c33823270d98daa65cb7c8
Sneha Paul <spaul1204@gmail.com>
2024-09-27 23:54:01 +0530

    created models user and property

---
+++ models/User.js
@@ -0,0 +1,27 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = Schema(
+  {
+    email: {
+      type: String,
+      unique: [true, "Email lready exists"],
+      equired: [true, "Email is required"],
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+    image: String,
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: Property,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
